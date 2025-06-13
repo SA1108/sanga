14bcc51c742daba3a47ca4fd564eb13128746576
SA1108 <yoron.home@gmail.com>
2025-06-13 16:04:07 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -96,6 +96,14 @@
 
     }; // end ssMobileMenu
 
+    $(".hov-anim").mouseover(function() {
+  $(this).attr("src", $(this).data("animated"))
+}),
+$(".hov-anim").mouseout(function() {
+  $(this).attr("src", $(this).data("static"))
+});
+
+
 
     /* search
     * ------------------------------------------------------ */
